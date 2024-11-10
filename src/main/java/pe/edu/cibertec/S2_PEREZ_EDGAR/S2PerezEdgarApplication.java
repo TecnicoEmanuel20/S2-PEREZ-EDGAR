09d509d4ea863d3993254e9faeba782a614f3c48
TecnicoEmanuel20 <perezanchayaedgaremanuel@gmail.com>
2024-11-10 10:13:40 -0500

    Primer commit del proyecto

---
+++ src/main/java/pe/edu/cibertec/S2_PEREZ_EDGAR/S2PerezEdgarApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.S2_PEREZ_EDGAR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S2PerezEdgarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S2PerezEdgarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/S2_PEREZ_EDGAR/S2PerezEdgarApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.S2_PEREZ_EDGAR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S2PerezEdgarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
